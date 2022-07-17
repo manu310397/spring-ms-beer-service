0e6b42759312d52206e24c57694bbc065c0d56b1
manu310397 <manojb912@gmail.com>
2022-07-17 15:40:58 +0530

    Initial commit

---
+++ src/main/java/com/manoj/springms/SpringMsBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.manoj.springms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMsBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMsBeerServiceApplication.class, args);
+    }
+
+}
