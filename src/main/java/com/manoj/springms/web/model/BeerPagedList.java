3db5bdcfc76ebd391f7000e1fa2ced28b553c5df
manu310397 <manojb912@gmail.com>
2022-07-17 16:08:27 +0530

    Added BeerDTO and Beer pages list

---
+++ src/main/java/com/manoj/springms/web/model/BeerPagedList.java
@@ -0,0 +1,17 @@
+package com.manoj.springms.web.model;
+
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class BeerPagedList extends PageImpl<BeerDTO> implements Serializable {
+    public BeerPagedList(List<BeerDTO> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public BeerPagedList(List<BeerDTO> content) {
+        super(content);
+    }
+}
---
+++ src/main/java/com/manoj/springms/web/model/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package com.manoj.springms.web.model;
+
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
